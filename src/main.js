3f5b5f532ed54a15194bba7c814b7d83c195e54f
msz <mszkb@skca.eu>
2018-10-31 10:10:27 +0100

    [FT] fn - rename all object keys with keys from given list, fn - intersect array and intersect each array of an array

--- src/main.js
+++ src/main.js
@@ -96,6 +96,100 @@ class libarr {
     static arr_clear(Array1) {
         Array1.length = 0
     }
+
+    /**
+     * Takes an object and returns an array of the values
+     * Specify the depth for recursive array creation
+     *
+     * Depth -1 => infinite, checks every child element and makes an array out of it
+     *
+     * @param Object - a plain object
+     * @param depth - how deep should the array creation go
+     */
+    static arr_objectToArray(object, depth = 1) {
+        // TODO depth
+        return Object.entries(object)
+    }
+
+    /**
+     * Takes a list of labels with keys and replace every property label
+     *
+     * @precondision:  keys length === object.keys length
+     *
+     * @param keys - array or object containing the labels
+     * @param arrayWithObjects
+     * @return object with all properties renamed with key list elements
+     */
+    static arr_replaceKeysInArrayWithObjects(keys, arrayWithObjects) {
+
+        let newArray = []
+
+        arrayWithObjects.forEach((element, i) => {
+            let single = {}
+            Object.keys(element).forEach((f, j) => {
+                single[keys[j]] = element[f]
+            })
+            newArray.push(single)
+        })
+
+        return newArray
+    }
+
+    /**
+     * Takes a list of labels with keys and wrap the array element
+     *
+     * @param keys
+     * @param array
+     */
+    static arr_addKeyToArray(keys, array) {
+
+    }
+
+    /**
+     * Intersects each value of one array with another array
+     *
+     * @precondision Only works with 1 dimension \n Arrays must not be nested
+     * @note Array values should be primitive
+     *
+     * @param Array1 - mixed content
+     * @param Array2 - mixed content
+     * @returns Array with intersected values
+     */
+    static arr_intersect(Array1, Array2) {
+        return Array1.filter(value => -1 !== Array2.indexOf(value));
+    }
+
+    /**
+     * Intersects every array of given array
+     * Only works with 1 nested level
+     *
+     * For deeper intersection @use arr_deepIntersectAll(ArrayOfDeppArrays)
+     *
+     * @param ArrayOfArrays - array with elements which are arrays with primitive types
+     * @returns Array with intersected values
+     */
+    static arr_intersectAll(ArrayOfArrays) {
+        let ArrayOfArraysLength = ArrayOfArrays.length
+
+        // Catch all the easy stuff
+        if (ArrayOfArraysLength === 0) {
+            return []  // given array is empty
+        } else if (ArrayOfArraysLength === 1) {
+            return ArrayOfArrays[0]  // given array has only one index
+        } else if (ArrayOfArraysLength === 2) {
+            return this.arr_intersect(ArrayOfArrays[0], ArrayOfArrays[1])  // only two indezes given, no need for the loop
+        }
+
+        // first round - initialize intersected array with the first two indezes
+        let intersected = this.arr_intersect(ArrayOfArrays[0], ArrayOfArrays[1])
+
+        // loop round - intersect every index of the array with the already intersected one
+        for (let idx = 2; idx <= ArrayOfArraysLength - 1; idx++) {
+            intersected = this.arr_intersect(intersected, ArrayOfArrays[idx])
+        }
+
+        return intersected
+    }
 }
 
 
--- src/main.test.js
+++ src/main.test.js
@@ -2,7 +2,7 @@ import libarr from './main.js';
 
 describe('libarr', () => {
   it('move elements from A to B', () => {
-    let array1 = [0,1,2,3,4,5,6,7,8,9]
+    let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     let array2 = []
 
     libarr.arr_moveTo(array1, array2)
@@ -15,7 +15,7 @@ describe('libarr', () => {
   });
 
   it('copy elements from A to B', () => {
-    let array1 = [0,1,2,3,4,5,6,7,8,9]
+    let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     let array2 = []
 
     libarr.arr_copyTo(array1, array2)
@@ -31,9 +31,9 @@ describe('libarr', () => {
   });
 
   it('move elements from A to B only the elements from A exists in C', () => {
-    let array1 = [0,1,2,3,4,5,6,7,8,9]
+    let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     let array2 = [1]
-    let array3 = [3,4,7]
+    let array3 = [3, 4, 7]
 
     libarr.arr_moveToOf(array1, array2, array3)
 
@@ -47,8 +47,8 @@ describe('libarr', () => {
   });
 
   it('remove elements from A which exists in C', () => {
-    let array1 = [0,1,2,3,4,5,6,7,8,9]
-    let array2 = [3,4,7]
+    let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let array2 = [3, 4, 7]
 
     libarr.arr_removeOf(array1, array2)
 
@@ -58,7 +58,7 @@ describe('libarr', () => {
   });
 
   it('remove all elements from A', () => {
-    let array1 = [0,1,2,3,4,5,6,7,8,9]
+    let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     libarr.arr_clear(array1)
 
@@ -81,7 +81,7 @@ describe('libarr', () => {
         name: "test4",
         value: 4,
         flag: false
-      },{
+      }, {
         name: "test5",
         value: 5,
         flag: false
@@ -91,7 +91,7 @@ describe('libarr', () => {
         value: 6,
         flag: false
       }
-      ]
+    ]
     let array2 = []
 
     libarr.arr_moveTo(array1, array2)
@@ -110,7 +110,7 @@ describe('libarr', () => {
           name: "anton",
           lastname: "tirol"
         },
-        values: [1,2,3],
+        values: [1, 2, 3],
         flags: {
           a: true,
           b: true
@@ -121,7 +121,7 @@ describe('libarr', () => {
           name: "anton1",
           lastname: "tirol1"
         },
-        values: [9,8,7],
+        values: [9, 8, 7],
         flags: {
           a: false,
           b: false
@@ -139,4 +139,86 @@ describe('libarr', () => {
     expect(array2[0].flags.a).toBeTruthy()
     expect(array2[1].person.name).toBe("anton1")
   });
+
+  it('create an array out of an object, depth 1', () => {
+    let object = {
+      0: ['2018-05-01', '0,58', '2018-05-01', '101', 'AT', '1871501', '114', '1', '0,58', '0', 'EUR', '0', 'False'],
+      1: ['2018-05-01', '61134,14', '2018-05-01', '101', 'AT', '1871501', '101', '1', '53160,1', '11', 'EUR', '5847,6', 'True']
+    }
+
+    let array = []
+    array = libarr.arr_objectToArray(object)  // depth 1 is default, so need for satisfy second param
+
+    expect(array.length).toBe(2)
+    expect(array[0][0]).toBe('2018-05-01')
+    expect(array[1][1]).toBe('61134,14')
+  })
+
+  it('create an array out of an object, depth 2', () => {
+    let object = {
+      cases: [
+        {
+          0: ['2018-05-01', '0,58', '2018-05-01', '101', 'AT', '1871501', '114', '1', '0,58', '0', 'EUR', '0', 'False'],
+          1: ['2018-05-01', '61134,14', '2018-05-01', '101', 'AT', '1871501', '101', '1', '53160,1', '11', 'EUR', '5847,6', 'True']
+        },
+        {
+          0: ['2018-07-31', '0', '2018-08-01', '101', 'AT', '1871701', '103', '2', '-137,4', '0', 'EUR', '0', 'False'],
+          1: ['2018-07-31', '0', '2018-08-01', '101', 'AT', '1871701', '103', '2', '13329,97', '4', 'EUR', '533,18', 'True']
+        },
+        {
+          0: ['2018-08-01', '0,58', '2018-08-01', '101', 'AT', '1871501', '114', '1', '0,58', '0', 'EUR', '0', 'False'],
+          1: ['2018-07-31', '10,83', '2018-08-01', '101', 'AT', '1871501', '114', '1', '9,4', '11', 'EUR', '1,04', 'True']
+        }
+      ]
+    }
+  })
+
+  it('replace property labels with labels in keys', () => {
+    let keys = ['BLDAT', 'BRUTTO']
+
+    let arrayWithObjects =
+      [
+        {
+          0: '2018-05-01',
+          1: '61134,14'
+        },
+         {
+          0: '2019-05-01',
+          1: '71134,14'
+        },
+         {
+          0: '2020-05-01',
+          1: '81134,14'
+        }
+      ]
+
+    let newObject = {}
+    newObject = libarr.arr_replaceKeysInArrayWithObjects(keys, arrayWithObjects)
+
+    expect(newObject[0].BLDAT).toBe('2018-05-01')
+
+  })
+
+
+  it('intersect 2 arrays', () => {
+    let array1 = [1,2,3,4,5,6,7,'a','b','c','d']
+    let array2 = [2,3,4,6,'c','x']
+
+    const intersect = libarr.arr_intersect(array1, array2)
+    expect(intersect[0]).toBe(2)
+  })
+
+
+  it('intersect 4 arrays inside an array', () => {
+    let array1 = [1,2,3,4,5,6,7,'a','b','c','d']
+    let array2 = [2,3,4,6,'c','d']
+    let array3 = [2,3,6,'c',1,'a','d']
+    let array4 = [2,3,4,'d']
+
+    let masterarray = [array1, array2, array3, array4]
+
+    const intersect = libarr.arr_intersectAll(masterarray)
+    expect(intersect).toEqual([2,3,'d'])
+  })
+
 });
